3b3f8166f938d0d1942ed46fd173ff1f41887975
george3262 <30941503+jptraber@users.noreply.github.com>
2023-10-21 13:32:53 -0700

    minor fixes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,37 +4,44 @@
 
     let inputText = "";
 
-    function searchActs(){
-        searchActsInput.set(inputText);
-        passActContent.set({});
+    function searchActs() {
+        if (inputText !== "") {
+            searchActsInput.set(inputText);
+            passActContent.set({});
+        }
     };
 
-    function searchHansards(){
-        searchHansardsInput.set(inputText);
-        passActContent.set({});
+    function searchHansards() {
+        if (inputText !== "") {
+            searchHansardsInput.set(inputText);
+            passActContent.set({});
+        }
     };
-
-    </script>
+</script>
 
 <AppShell>
-        <div class="vertical-center">
-            <div class="container mx-auto mt-10 mb-6 px-5">
-                <div style="display: flex; flex-direction: column; align-items: center;">
+    <div class="vertical-center">
+        <div class="container mx-auto mt-10 mb-6 px-5">
+            <div style="display: flex; flex-direction: column; align-items: center;">
                 <textarea bind:value={inputText} 
                     class="textarea p-3 border border-primary-500" rows="1" 
                     placeholder="search" 
                     style="width: 70%; min-width: 450px;"></textarea>
                 <div class="button-container">
-                    <button class="btn btn-lg variant-filled-secondary m-3" on:click={searchActs}>
+                    <button class="btn btn-lg variant-filled-secondary m-3" 
+                        on:click={searchActs}
+                        {inputText === "" ? 'disabled' : ''}>
                         <a href="/acts/search">Search Acts</a>
                     </button>
-                    <button class="btn btn-lg variant-filled-secondary m-3" on:click={searchHansards}>
+                    <button class="btn btn-lg variant-filled-secondary m-3" 
+                        on:click={searchHansards}
+                        {inputText === "" ? 'disabled' : ''}>
                         <a href="/hansards">Search Hansards</a>
                     </button>
                 </div>
             </div>
         </div>
-
+    </div>
 </AppShell>
 
 <style>
